a6409b8facc98aa029c9a4f4b003f6e4647f34fc
felipyroma <felipy.roma@sptech.school>
2022-11-04 15:21:55 -0300

    att

--- Banco de dados/Systoyanka.sql
+++ Banco de dados/Systoyanka.sql
@@ -1,60 +1,60 @@
 create database systoyanka;
 use systoyanka;
 
-create table Shopping (
+create table shopping (
 idshopping int primary key auto_increment,
-Nome varchar(45),
-CNPJ char(14) unique,
-Telefone char (10)
+nome varchar(45),
+cnpj char(14) unique,
+tel char (10)
 );
 
 
-Create table Endereço (
+Create table endereco (
 idEndereço int primary key auto_increment,
-Cidade varchar(45),
-Bairro varchar (45),
-CEP varchar (9),
+cidade varchar(45),
+bairro varchar (45),
+cep varchar (9),
  fkshoppingE int,
  constraint foreign key fkshoppingE (fkshoppingE) references Shopping (idshopping) 
 );
 
 
-create table Sensor (
+create table sensor (
 idSensor int primary key auto_increment,
-Andar char(2),
-Bloco varchar (4),
+andar char(2),
+bloco varchar (4),
 fkShoppingS int,constraint foreign key fkshoppingS (fkshoppingS) references Shopping (idshopping)
 );
 
-create table Leitura(
+create table leitura (
 idResposta int auto_increment,
-Dt_hora DATETIME, 
+dt_hora DATETIME, 
 statusRetorno int,
 fkSensor int,constraint foreign key fkSensor (fkSensor) references Sensor (idSensor),
 primary key (idResposta, fkSensor)
 );
 
-create table Usuario (
+create table usuario (
 idUsuario int auto_increment,
-Login varchar(45),
-Senha varchar(45),
-Tipo varchar(45), constraint chkTipo 
+login varchar(45),
+senha varchar(45),
+tipo varchar(45), constraint chkTipo 
 check (tipo in ('Admin','user')),
 fkShopping int,constraint foreign key fkShopping (fkShopping) references Shopping (idshopping),
 primary key (idUsuario, fkShopping)
 );
 
-insert into Shopping (Nome, CNPJ, Telefone) values
+insert into shopping (nome, cnpj, telefone) values
  ('Shopping Praça da moça', '52678945301254', '1185461247'),
  ('Shopping Aricanduva', '24355214544132', '1140674545'),
  ('Shopping JK Iguatemi', '64589092093425', '1140664066');
 
-insert into Endereço (cidade, Bairro, CEP, fkshoppingE) values
+insert into endereco (cidade, bairro, cep, fkshoppingE) values
  ( 'Diadema', 'Praça Da Moça','09910-720', 1),
 ('São Paulo', 'aricanduva','03527-900', 2),
 ('São Paulo', 'Vila Olímpia','04543-011', 3);
 
-insert into sensor (Andar, Bloco, fkshoppingS) values
+insert into sensor (andar, bloco, fkshoppingS) values
 (01,'A',1),
 (01,'A',2),
 (01,'A',3),
@@ -92,7 +92,7 @@ insert into sensor (Andar, Bloco, fkshoppingS) values
 (03,'D',2),
 (03,'D',3);
 
-insert into leitura (Dt_hora, Retorno, fksensor) values
+insert into leitura (dt_hora, statusRetorno, fksensor) values
 ('2022-10-23 19:50:00', 1, 1),
 ('2022-10-22 18:43:00', 1, 2),
 ('2022-10-20 14:15:00', 0, 3),
@@ -114,30 +114,42 @@ select * from leitura;
 
 select * from usuario;
 
-SELECT SUM(Retorno) FROM leitura;
+select SUM(statusRetorno) 'Soma dos retornos' from leitura;
 
-SELECT * FROM shopping JOIN endereço 
-	ON idShopping = fkShoppingE;
+select * from shopping
+	join endereco
+		on idShopping = fkShoppingE;
     
-SELECT * FROM shopping JOIN sensor 
-	ON idShopping = fkShoppingS;
+select * from shopping
+	join sensor 
+		on idShopping = fkShoppingS;
 
-SELECT * FROM sensor JOIN leitura 
-	ON idSensor = fkSensor;
+select * from sensor 
+	join leitura 
+		on idSensor = fkSensor;
     
-SELECT * FROM sensor JOIN leitura 
-	ON idSensor = fksensor;
+select * from sensor
+	join leitura 
+		on idSensor = fksensor;
     
-SELECT * FROM shopping JOIN endereço 
-	ON idShopping = fkShoppingE
-    join sensor on idShopping = fkShoppingS
-    join usuario on idShopping = fkShopping;
+select * from shopping
+	join endereço 
+		on idShopping = fkShoppingE
+    join sensor
+		on idShopping = fkShoppingS
+    join usuario
+		on idShopping = fkShopping;
     
-    SELECT * FROM shopping JOIN endereço 
-	ON idShopping = fkShoppingE
-    join sensor on idShopping = fkShoppingS
-    join usuario on idShopping = fkShopping
-    where cidade like 'D%';
+    select * from shopping
+		join endereço 
+			on idShopping = fkShoppingE
+		join sensor
+			on idShopping = fkShoppingS
+		join usuario
+			on idShopping = fkShopping
+		where cidade like 'D%';
+        
+drop database systoyanka;
     
     
     
